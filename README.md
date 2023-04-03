b576884574a805bc0ce613cae464e284082aea0a
rawbin.zhang <rawbin.zhang@lunarax.io>
2023-04-03 20:55:13 +0800

    add doc and server to cors allow

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # okta-cors-repo
+## localhost:8002 
+- mock for the app
+- client api request is from this origin and they are reverse proxy to gateway mock localhost:10001
+
+## localhost:10001
+- mock for the gateway
+
+## localhost:20002
+- mock for the okta server
--- okta-mock/index.js
+++ okta-mock/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 http.createServer((request,response) => {
     console.log('okta-mock:',request.url)
     response.writeHead(200,{
-        // "Access-Control-Allow-Origin":"*",   // set this to allow cross origin
+        "Access-Control-Allow-Origin":"*",   // set this to allow cross origin
         "Access-Control-Allow-Methods":'GET,POST,HEAD,DELETE,PUT',
         "Content-Type": 'application/json'
     });
